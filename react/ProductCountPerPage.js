9b4ef8341b30f885fe4c58536dbc4ef736baaa37
marcosewbank <ewbankmarcos@gmail.com>
2020-03-19 16:49:20 -0300

    finish bar

--- react/ProductCountPerPage.js
+++ react/ProductCountPerPage.js
@@ -5,10 +5,13 @@ import { FormattedMessage } from 'react-intl'
 import { useSearchPage } from 'vtex.search-page-context/SearchPageContext'
 import { useCssHandles } from 'vtex.css-handles'
 
+import styles from './searchResult.css'
+
 const CSS_HANDLES = ['showingProducts', 'showingProductsCount']
 
 const ProductCountPerPage = () => {
   const { searchQuery } = useSearchPage()
+
   const handles = useCssHandles(CSS_HANDLES)
   const products =
     path(['data', 'productSearch', 'products'], searchQuery) || []
@@ -17,30 +20,44 @@ const ProductCountPerPage = () => {
     searchQuery
   )
 
+  const productsLoadedPercentage = Math.round(
+    (100 * products.length) / recordsFiltered
+  )
+  console.log('productCountPercentage', productsLoadedPercentage)
   if (products.length === 0) {
     return null
   }
 
   return (
-    <div className={`${handles.showingProducts} tc t-small pt3 c-muted-2`}>
-      <FormattedMessage
-        id="store/search-result.showing-products"
-        tagName="span"
-        values={{
-          value: (
-            <span className={`${handles.showingProductsCount} b`}>
-              <FormattedMessage
-                id="store/search-result.showing-products-count"
-                values={{
-                  productsLoaded: products.length,
-                  total: recordsFiltered,
-                }}
-              />
-            </span>
-          ),
-        }}
-      />
-    </div>
+    <>
+      <div className={`${handles.showingProducts} tc t-small pt3 c-muted-2`}>
+        <FormattedMessage
+          id="store/search-result.showing-products"
+          tagName="span"
+          values={{
+            value: (
+              <span className={`${handles.showingProductsCount} b`}>
+                <FormattedMessage
+                  id="store/search-result.showing-products-count"
+                  values={{
+                    productsLoaded: products.length,
+                    total: recordsFiltered,
+                  }}
+                />
+              </span>
+            ),
+          }}
+        />
+      </div>
+      {productsLoadedPercentage && (
+        <div className={`${styles.progressBar} relative flex items-center`}>
+          <span
+            className={`${styles.filler} absolute`}
+            style={{ width: `${productsLoadedPercentage}%` }}
+          />
+        </div>
+      )}
+    </>
   )
 }
 
