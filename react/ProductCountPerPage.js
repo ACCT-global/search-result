c5494029df8067223dd73577c9de8d151cd4c31a
marcosewbank <ewbankmarcos@gmail.com>
2020-03-20 11:19:22 -0300

    Finish

--- react/ProductCountPerPage.js
+++ react/ProductCountPerPage.js
@@ -5,9 +5,14 @@ import { FormattedMessage } from 'react-intl'
 import { useSearchPage } from 'vtex.search-page-context/SearchPageContext'
 import { useCssHandles } from 'vtex.css-handles'
 
-import styles from './searchResult.css'
-
-const CSS_HANDLES = ['showingProducts', 'showingProductsCount']
+const CSS_HANDLES = [
+  'showingProducts',
+  'showingProductsCount',
+  'showingProductsContainer',
+  'showingAllProductsCount',
+  'progressBar',
+  'progressBarFiller',
+]
 
 const ProductCountPerPage = () => {
   const { searchQuery } = useSearchPage()
@@ -23,41 +28,63 @@ const ProductCountPerPage = () => {
   const productsLoadedPercentage = Math.round(
     (100 * products.length) / recordsFiltered
   )
-  console.log('productCountPercentage', productsLoadedPercentage)
   if (products.length === 0) {
     return null
   }
 
   return (
-    <>
+    <div
+      className={`${handles.showingProductsContainer} flex flex-column justify-center items-center`}
+    >
       <div className={`${handles.showingProducts} tc t-small pt3 c-muted-2`}>
-        <FormattedMessage
-          id="store/search-result.showing-products"
-          tagName="span"
-          values={{
-            value: (
-              <span className={`${handles.showingProductsCount} b`}>
-                <FormattedMessage
-                  id="store/search-result.showing-products-count"
-                  values={{
-                    productsLoaded: products.length,
-                    total: recordsFiltered,
-                  }}
-                />
-              </span>
-            ),
-          }}
-        />
+        {productsLoadedPercentage !== 100 ? (
+          <FormattedMessage
+            id="store/search-result.showing-products"
+            tagName="span"
+            values={{
+              value: (
+                <span className={`${handles.showingProductsCount} b`}>
+                  <FormattedMessage
+                    id="store/search-result.showing-products-count"
+                    values={{
+                      productsLoaded: products.length,
+                      total: recordsFiltered,
+                    }}
+                  />
+                </span>
+              ),
+            }}
+          />
+        ) : (
+          <FormattedMessage
+            id="store/search-result.showing-all-products"
+            tagName="span"
+            values={{
+              value: (
+                <span className={`${handles.showingAllProductsCount} b`}>
+                  <FormattedMessage
+                    id="store/search-result.showing-all-products-count"
+                    values={{
+                      total: recordsFiltered,
+                    }}
+                  />
+                </span>
+              ),
+            }}
+          />
+        )}
       </div>
       {productsLoadedPercentage && (
-        <div className={`${styles.progressBar} relative flex items-center`}>
+        <div
+          className={`${handles.progressBar} relative flex bg-action-secondary items-center mt4 br3 w-50 h1`}
+        >
           <span
-            className={`${styles.filler} absolute`}
+            className={`${handles.progressBarFiller} absolute br3 bg-action-primary h1`}
             style={{ width: `${productsLoadedPercentage}%` }}
           />
         </div>
       )}
-    </>
+    </div>
   )
 }
 
